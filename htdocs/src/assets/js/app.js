b2aab5b3a82da66f09f2c24d305c58fd1531fcf1
Dave Sayer <dave@bathdesign.co.uk>
2016-08-26 22:38:10 +0100

    a bit of responsive. nav items linked up and put reveal back to test speed stuff

--- htdocs/src/assets/js/app.js
+++ htdocs/src/assets/js/app.js
@@ -1,36 +1,36 @@
 $(document).foundation();
 
-// window.sr = ScrollReveal({ reset: true });
+window.sr = ScrollReveal({ reset: true });
 
-// var fooReveal = {
-//   delay    : 200,
-//   distance : '20px',
-//   easing   : 'ease-in-out',
-//   scale    : 1.1,
-//   duration: 600,
-//   viewFactor: 0.4
-// };
-// sr.reveal('#products .column', fooReveal);
+var fooReveal = {
+  delay    : 200,
+  distance : '20px',
+  easing   : 'ease-in-out',
+  scale    : 1.1,
+  duration: 600,
+  viewFactor: 0.4
+};
+sr.reveal('#products .column', fooReveal);
 
-// var copyReveal = {
-//   delay    : 500,
-//   distance : '10px',
-//   easing   : 'ease-in-out',
-//   scale    : 1.1,
-//   duration: 700,
-//   viewFactor: 0.1
-// };
-// sr.reveal('.callout', fooReveal);
+var copyReveal = {
+  delay    : 500,
+  distance : '10px',
+  easing   : 'ease-in-out',
+  scale    : 1.1,
+  duration: 700,
+  viewFactor: 0.1
+};
+sr.reveal('.callout', fooReveal);
 
-// var footerReveal = {
-//   delay    : 500,
-//   distance : '10px',
-//   easing   : 'ease-in-out',
-//   scale    : 1.1,
-//   duration: 700,
-//   viewFactor: 0.1
-// };
-// sr.reveal('footer li', fooReveal);
+var footerReveal = {
+  delay    : 500,
+  distance : '10px',
+  easing   : 'ease-in-out',
+  scale    : 1.1,
+  duration: 700,
+  viewFactor: 0.1
+};
+sr.reveal('footer li', fooReveal);
 
 // function parallax() {
 //     var jarallax = new Jarallax();
